c21513c2c5fd601d180b68e20e9274a249618f06
mischiefluis <loidatolentino358@gmail.com>
2024-03-15 22:51:48 +0800

    dbconnect test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 function App() {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/').then((res) => {
+      console.log(res.data);
+      setList(res.data)
+    })
+  }, [])
+
   return (
     <div>
-      <h1 className="text-4xl text-green-600">Hello</h1>
+      {list.map((item) => {
+        return (
+          <h1 key={item.id}>{item.name}</h1>
+        )
+      })}
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+const mysql = require("mysql")
+
+app.use(cors())
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test"
+})
+
+
+app.get("/", (req,res) => {
+  const sql = "select * from testtable"
+  db.query(sql, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json(data)
+  })
+})
+
+
+app.get("/", (req, res) => {
+  res.json({"msg":"Hello"})
+})
 
 app.listen(3001, () => {
   console.log("Server running on port 3001");
-})
-
-app.get("/", (req,res) => {
-  res.send("Hello World")
 })
